48b727fa83809f583bda503a06109be8e300004a
Tahmid Hassan Khan <tahmidxp96@Tahmids-MacBook-Air.local>
2026-05-21 23:16:02 +0600

    fix(kindle): fix dictionary recognition metadata & cleanup heavy installers

--- kindle_dictionary_builder.py
+++ kindle_dictionary_builder.py
@@ -713,7 +713,7 @@ class KindleDictionaryCompiler:
                 f.write("""<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"
-      xmlns:dx="http://www.amazon.com/xmlns/idx"
+      xmlns:idx="http://www.amazon.com/xmlns/idx"
       xmlns:mbp="http://www.amazon.com/xmlns/mbp"
       xmlns:mm="http://www.amazon.com/xmlns/mobi">
 <head>
@@ -764,10 +764,12 @@ class KindleDictionaryCompiler:
     <dc:publisher>Shobdo Vandar Project</dc:publisher>
     
     <!-- Kindle Dictionary Metadata Declarations -->
-    <meta name="BookType" content="Dictionary"/>
-    <meta name="x-mobi-dictionary-input-language" content="{self.input_lang}"/>
-    <meta name="x-mobi-dictionary-output-language" content="{self.output_lang}"/>
-    <meta name="x-mobi-dictionary-default-lookup-index" content="{self.index_name}"/>
+    <meta name="BookType" content="dictionary"/>
+    <x-metadata>
+      <DictionaryInLanguage>{self.input_lang}</DictionaryInLanguage>
+      <DictionaryOutLanguage>{self.output_lang}</DictionaryOutLanguage>
+      <DefaultLookupIndex>{self.index_name}</DefaultLookupIndex>
+    </x-metadata>
   </metadata>
   
   <manifest>
